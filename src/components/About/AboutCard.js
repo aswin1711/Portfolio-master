c62ea260a693670b3233c9c83bab256edb1fb94e
Aswin Sukumaran <aswinraina1711@gmail.com>
2025-02-09 14:26:24 +0530

    enter

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Aswin Sukumaran </span>
+            from <span className="purple"> Chennai, India.</span>
+            <br />
+            I am currently looking for opportunities as a Full Stack Developer
+            <br />
+            I have completed Electrical and Electronics Engineering from Hindustan University 
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Video Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Checking Tech Trends
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Aswin Sukumaran</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
